abbe142b833b4a36bc9343b5559008bed08dc791
citissime <maxhaggqvist@gmail.com>
2021-02-19 18:48:30 +1100

    filter areas

--- citoLibrary.js
+++ citoLibrary.js
@@ -1,3 +1,11 @@
+const QUERY_SELECTORS = {
+
+    AREA_CITY: '#area-city'
+
+
+}
+
+
 function hello() { 
     var msg = {
         hello: 'hello world'
@@ -5,5 +13,16 @@ function hello() {
     return msg;
 };
 
+
+function filterAreas(city, areas) {
+
+    for (var i = 0; i < areas.length; i++) {
+        if (areas[i].querySelector(QUERY_SELECTORS.AREA_CITY).innerHTML != city) {
+            areas[i].style.display = 'hidden';
+        }
+    }
+
+    return areas;
+}
 
 
