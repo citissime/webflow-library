656d9066347c2e1fc842ff3ea1323ae3f3240091
citissime <maxhaggqvist@gmail.com>
2021-02-19 22:00:46 +1100

    fixed small error

--- citoLibrary.js
+++ citoLibrary.js
@@ -28,8 +28,7 @@ function filterShops(areaName, shopAreas, filteredShopContainer) {
     var areaNameList = document.getElementsByClassName(areaName);
     var shopAreas = document.getElementsByClassName(shopAreas);
     var filteredShopContainer = document.getElementById(filteredShopContainer);
-
-
+    var filteredShops = [];
 
     for (var i = 0; i < shopAreas.length; i++) {
 
@@ -39,10 +38,14 @@ function filterShops(areaName, shopAreas, filteredShopContainer) {
         for (var y = 0; y < areaArray.length; y++) {
             for (var x = 0; x < areaNameList.length; x++) {
                 if (areaNameList[x].innerHTML == areaArray[y]) {
-                    filteredShopContainer.appendChild(shopAreas[i].parentNode);
+                    filteredShops.push(shopAreas[i].parentNode);
                 }
             }
         }
     } 
+
+    for (var i = 0; i < filteredShops.length; i++) {
+        filteredShopContainer.appendChild(filteredShops[i]);
+    }
 }
 
